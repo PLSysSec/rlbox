e139e369a0a161293b5b1bafea27a03eb33908a2
shravanrn@gmail.com <shravanrn@gmail.com>
2019-08-14 20:38:53 -0700

    Update docs

--- docs/doxygen/html/search/functions_7.js
+++ docs/doxygen/html/search/functions_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['unsafe_5fsandboxed',['UNSAFE_sandboxed',['../classrlbox_1_1tainted__base__impl.html#adb4270e907768562120dc32cf955424e',1,'rlbox::tainted_base_impl::UNSAFE_sandboxed()'],['../classrlbox_1_1sandbox__callback.html#a6824744f81cf277e55d020f388419a47',1,'rlbox::sandbox_callback::UNSAFE_sandboxed()']]],
+  ['unsafe_5fsandboxed',['UNSAFE_sandboxed',['../classrlbox_1_1tainted__base__impl.html#ae2c69129cbb9344e7d2623129f031214',1,'rlbox::tainted_base_impl::UNSAFE_sandboxed()'],['../classrlbox_1_1sandbox__callback.html#a0a71e99d85bd22cd642327017c863b26',1,'rlbox::sandbox_callback::UNSAFE_sandboxed()']]],
   ['unsafe_5funverified',['UNSAFE_unverified',['../classrlbox_1_1tainted__base__impl.html#a01acab6b4bd8137afa03cf4b2678844f',1,'rlbox::tainted_base_impl::UNSAFE_unverified()'],['../classrlbox_1_1sandbox__callback.html#a9d7a4e08413d3989ce8633ea459b1110',1,'rlbox::sandbox_callback::UNSAFE_unverified()']]],
   ['unverified_5fsafe_5fbecause',['unverified_safe_because',['../classrlbox_1_1tainted__base__impl.html#a1292132a467d30150be6d4e2b72ff6b2',1,'rlbox::tainted_base_impl']]]
 ];
