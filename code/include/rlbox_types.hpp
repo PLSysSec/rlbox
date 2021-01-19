1f36175173fd4bf1dc93b469cfe72276d53143a4
shravanrn@gmail.com <shravanrn@gmail.com>
2021-01-18 22:44:35 -0800

    Add static indirect sandbox

--- code/include/rlbox_types.hpp
+++ code/include/rlbox_types.hpp
@@ -103,4 +103,6 @@ class rlbox_noop_sandbox;
 class rlbox_noopindirect_sandbox;
 
 class rlbox_dylib_sandbox;
+
+class rlbox_noopstaticindirect_sandbox;
 }
---
+++ code/tests/rlbox_glue/test_noopstaticindirect_sandbox_glue.cpp
@@ -0,0 +1,15 @@
+// NOLINTNEXTLINE
+#define RLBOX_USE_STATIC_CALLS() rlbox_noopstaticindirect_sandbox_lookup_symbol
+#define RLBOX_USE_EXCEPTIONS
+#define RLBOX_ENABLE_DEBUG_ASSERTIONS
+#define RLBOX_SINGLE_THREADED_INVOCATIONS
+#include "rlbox_noopstaticindirect_sandbox.hpp"
+
+// NOLINTNEXTLINE
+#define TestName "rlbox_noopstaticindirect_sandbox"
+// NOLINTNEXTLINE
+#define TestType rlbox::rlbox_noopstaticindirect_sandbox
+// NOLINTNEXTLINE
+#define CreateSandbox(sandbox) sandbox.create_sandbox()
+
+#include "test_sandbox_glue.inc.cpp"
