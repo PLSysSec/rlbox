a441977c8ff2da6626a3ff1a3cd2c6d0a9a9bed5
shravanrn@gmail.com <shravanrn@gmail.com>
2021-10-14 17:15:31 -0700

    Remove print when custom abort exists

--- code/include/rlbox_helpers.hpp
+++ code/include/rlbox_helpers.hpp
@@ -24,10 +24,10 @@ namespace detail {
     #if __cpp_exceptions && defined(RLBOX_USE_EXCEPTIONS)
       throw std::runtime_error(msg);
     #else
-      std::cerr << msg << std::endl;
       #ifdef RLBOX_CUSTOM_ABORT
         RLBOX_CUSTOM_ABORT(msg);
       #else
+        std::cerr << msg << std::endl;
         std::abort();
       #endif
     #endif
