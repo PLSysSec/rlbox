18639e7d2c2c8fe6fa39e6ace5c4a26c05f1802d
shravanrn@gmail.com <shravanrn@gmail.com>
2022-04-27 15:28:55 -0700

    Format iwyu

--- iwyu_wrapper/iwyu_wrapper.py
+++ iwyu_wrapper/iwyu_wrapper.py
@@ -5,11 +5,13 @@ import subprocess
 import os
 import sys
 
+
 def format_output(str):
-    str = str.decode('UTF-8').strip()
-    if " has correct #includes/fwd-decls" in str or str == '':
+    str = str.decode("UTF-8").strip()
+    if " has correct #includes/fwd-decls" in str or str == "":
         return None
     return str
+
 
 def execute_iwyu(cmd):
     p = subprocess.Popen(["iwyu"] + cmd, stdout=PIPE, stderr=PIPE)
@@ -25,11 +27,13 @@ def execute_iwyu(cmd):
 
     return p.returncode
 
+
 def get_iwyu_default_return():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     test_file = os.path.join(dir_path, "test_iwyu.cpp")
     returncode = execute_iwyu([test_file])
     return returncode
+
 
 def split_args(args):
     no_error = False
@@ -37,14 +41,15 @@ def split_args(args):
         args.remove("--no-error")
         no_error = True
 
-    files_index = [i for i, v in enumerate(args) if v == '--']
+    files_index = [i for i, v in enumerate(args) if v == "--"]
     if len(files_index) == 0:
         return no_error, args, []
     elif len(files_index) == 1:
-        return no_error, args[:files_index[0]], args[files_index[0]+1:]
+        return no_error, args[: files_index[0]], args[files_index[0] + 1 :]
     else:
         sys.stderr.write("Found more than one instance of --")
         exit(10)
+
 
 def main():
     args = sys.argv[1:]
@@ -57,7 +62,8 @@ def main():
         returncode = execute_iwyu(iwyu_args + [file])
 
         if no_error == False:
-            if (returncode != default_ret):
+            if returncode != default_ret:
                 exit(returncode)
+
 
 main()
--- iwyu_wrapper/test_iwyu.cpp
+++ iwyu_wrapper/test_iwyu.cpp
@@ -1,5 +1,6 @@
-int main(int argc, char** argv) {
-    (void) argc;
-    (void) argv;
-    return 0;
+int main(int argc, char** argv)
+{
+  (void)argc;
+  (void)argv;
+  return 0;
 }
