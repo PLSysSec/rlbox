7e512422281216a9f6f921087d0fc85204289968
Shravan Narayan <shravanrn@gmail.com>
2024-01-03 13:58:11 -0600

    Switch to templated class

--- tests/rlbox_unit_tests/test_forwarder.cpp
+++ tests/rlbox_unit_tests/test_forwarder.cpp
@@ -15,65 +15,43 @@
 #include "rlbox_helpers.hpp"
 #include "rlbox_type_traits.hpp"
 
-class equals_test_subclass : public std::optional<int> {
+class optional_test_subclass : public std::optional<int> {
  public:
 #define RLBOX_FORWARD_TARGET_CLASS std::optional<int>
-#define RLBOX_FORWARD_CURR_CLASS equals_test_subclass
+#define RLBOX_FORWARD_CURR_CLASS optional_test_subclass
 #define RLBOX_FORWARD_TO_SUBCLASS
 
 #include "rlbox_forwarder.hpp"
 };
 
-TEST_CASE("rlbox_forwarder operater (not) equals works correctly for subclass",
-          "[rlbox_forwarder]") {
-  std::optional<int> a1 = 3;
-  std::optional<int> a2 = 4;
-
-  int i1 = 3;
-  int i2 = 4;
-
-  equals_test_subclass f1 = 3;
-  equals_test_subclass f1b = 3;
-  equals_test_subclass f2 = 4;
-
-  REQUIRE(f1 == f1b);
-
-  REQUIRE(f1 == a1);
-  REQUIRE(f1 == i1);
-
-  REQUIRE(a1 == f1);
-  REQUIRE(i1 == f1);
-
-  REQUIRE(f1 != f2);
-
-  REQUIRE(f1 != a2);
-  REQUIRE(f1 != i2);
-
-  REQUIRE(a1 != f2);
-  REQUIRE(i1 != f2);
-}
-
-class equals_test_member {
+class optional_test_member {
  public:
   std::optional<int> m;
 #define RLBOX_FORWARD_TARGET_CLASS std::optional<int>
-#define RLBOX_FORWARD_CURR_CLASS equals_test_member
+#define RLBOX_FORWARD_CURR_CLASS optional_test_member
 #define RLBOX_FORWARD_TO_OBJECT m
 
 #include "rlbox_forwarder.hpp"
 };
 
-TEST_CASE("rlbox_forwarder operater (not) equals works correctly for member",
-          "[rlbox_forwarder]") {
+TEMPLATE_TEST_CASE("rlbox_forwarder constructor works correctly",
+                   "[rlbox_forwarder]", optional_test_subclass,
+                   optional_test_member) {
+  [[maybe_unused]] TestType t;
+}
+
+TEMPLATE_TEST_CASE("rlbox_forwarder operater (not) equals works correctly",
+                   "[rlbox_forwarder]", optional_test_subclass,
+                   optional_test_member) {
   std::optional<int> a1 = 3;
   std::optional<int> a2 = 4;
 
   int i1 = 3;
   int i2 = 4;
 
-  equals_test_member f1 = 3;
-  equals_test_member f1b = 3;
-  equals_test_member f2 = 4;
+  TestType f1 = 3;
+  TestType f1b = 3;
+  TestType f2 = 4;
 
   REQUIRE(f1 == f1b);
 
